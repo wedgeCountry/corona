adjusted fitting to sir model and fixed scaling

fitting.py
@@ -8,63 +8,122 @@ import datetime
 import pandas as pd
 
 from models.SEIR import SEIR
+from models.SIR import SIR
 from prepare_data import load_data_germany
+from datetime import date
+
+def which(boolean_array):
+    for i, b in enumerate(boolean_array):
+        if b:
+            yield i
 
 if __name__ == "__main__":
 
-    N = 83200000.   # Einwohnerzahl von Deutschland 2019/2020
-    r0 = 2.4        # Ansteckungsrate
-    gamma = 1 / 3   # Mittlere Infektiöse Zeit
-    beta = r0 * gamma
-    a = 1 / 5.5     # 1/a Latenzzeit
 
-    # scipy.optimize.curve_fit(f, xdata, ydata, p0=None, sigma=None, absolute_sigma=False, check_finite=True,
-    #                         bounds=(-inf, inf), method=None, jac=None, **kwargs)[source]¶
+    data_ger = pd.read_csv("data/Hopkins_timeseries_Germany.csv", sep=";")
 
-    data = load_data_germany()
-
-    T_data = np.array(data["T"])
-    Y_data = np.array(data["cases"]) / N
-    print(T_data)
-    print(Y_data)
+    data_ger["date"] = [date(2000 + int(d.split("/")[2]),
+          int(d.split("/")[0]),
+          int(d.split("/")[1])) for d in data_ger["T"]]
+    data_ger["confirmed"] = data_ger["confirmed.V1"]
+    data_ger["recovered"] = data_ger["recovered.V1"]
+    data_ger["dead"] = data_ger["dead.V1"]
     #exit(0)
-    T_min, T_max = min(T_data), max(T_data)
-    # T_min, T_max = data["T"].min(), 25
-    #T_min, T_max = 25, data["T"].max()
-    IR0 = min(data.cases[T_min:T_max])
-    E0, I0, R0 = 0., (1. / 3.) * IR0 / N, (2. / 3.) * IR0 / N
-    y0 = [1. - E0 - I0 - R0, E0, I0, R0]
-    print("IR0", IR0)
 
+    #print(T_data)
+    days = np.array([td.days for td in data_ger["date"] - min(data_ger["date"])])
+    confirmed = np.array(data_ger["confirmed"])
+    recovered = np.array(data_ger["recovered"])
+    mindex = min(which(confirmed > 1000))
+
+    confirmed = confirmed[mindex:]
+    recovered = recovered[mindex:]
+    days = days[mindex:]
+    print("days", days)
+    print("confirmed", confirmed)
+
+    N = 83200000.  # Einwohnerzahl von Deutschland 2019/2020
+
+    R_data = recovered / N
+    I_data = (confirmed - recovered) / N
+
+    day_0 = days[0]
+    R0 = R_data[0]
+    I0 = I_data[0]
+    y0 = [1. - I0 - R0, I0, R0]
+
+    r0 = 1.2        # Ansteckungsrate
+    gamma = 1 / 3   # 1/gamma: Mittlere Infektiöse Zeit
+    beta = r0 * gamma
+
+    print("y0", y0)
 
     def create_model(_r0):
-        return SEIR(_r0 * gamma, gamma, a)
+        return SIR(_r0 * gamma, gamma)
 
-
-    def fit_func(t, _r0):
-        model = create_model(_r0)
-        T, Y = model.integrate(y0, min(t), max(t))
-        S, E, I, R = model.unravel(Y)
+    def fit_func(t, y0, _r0):
+        mdl = create_model(_r0)
+        ti, yi = mdl.integrate(y0, min(t), max(t))
+        S, I, R = mdl.unravel(yi)
         IpR = [i + r for i, r in zip(I, R)]
-        interpolation = scipy.interpolate.interp1d(T, IpR)
-        print([interpolation(ti) for ti in t])
-        return [interpolation(ti) for ti in t]
+        interpolation = scipy.interpolate.interp1d(ti, IpR)
+        return [interpolation(i) for i in t]
 
-    def obj(_r0):
-        Y_fitted = fit_func(T_data, _r0)
-        return np.sum(np.abs(Y_data - np.array(Y_fitted)))
+    y0_from_start = [1. - I0 - R0, I0, R0]
 
-    res = scipy.optimize.minimize(obj, [r0])
+    def obj_from_start(_r0, t_input, y_input):
+        y_fitted = fit_func(t_input, y0_from_start, _r0)
+        return max(np.abs(y_input - np.array(y_fitted)))
+
+    res_from_start = scipy.optimize.minimize(lambda x: obj_from_start(x, days[1:10], I_data[1:10]), [r0])
+
+    y0_ab_day_65 = [1. - I_data[(65 - day_0)] - R_data[(65 - day_0)], I_data[(65 - day_0)], R_data[(65 - day_0)]]
+    print(y0_ab_day_65)
+
+    def obj_ab_day_65(_r0):
+        t_input = days[(65 - day_0):]
+        y_input = I_data[(65 - day_0):]
+        y_fitted = fit_func(t_input, y0_ab_day_65, _r0)
+        return sum((y_input - np.array(y_fitted))**2)
+
+    res_ab_day_65 = scipy.optimize.minimize(obj_ab_day_65, [1.2])
+
+    #exit(0)
+
+    to_day = max(days)
+    model_from_start = SIR(res_from_start.x * gamma, gamma)
+    T_from_start, Y_from_start = model_from_start.integrate(y0_from_start, min(days), to_day)
+    S_from_start, I_from_start, R_from_start = model_from_start.unravel(Y_from_start)
+
+    model_ab_day_65 = SIR(res_ab_day_65.x * gamma, gamma)
+    T_ab_day_65, Y_ab_day_65 = model_ab_day_65.integrate(y0_ab_day_65, days[(65 - day_0)], to_day)
+    S_ab_day_65, I_ab_day_65, R_ab_day_65 = model_ab_day_65.unravel(Y_ab_day_65)
+
+    fig, ax = pyplot.subplots()
+    ax.plot(T_from_start, N * I_from_start, "y")
+    ax.plot(T_from_start, N * R_from_start, "b")
+    ax.plot(T_from_start, N * (I_from_start + R_from_start), "k")
+   # ax.plot(T_ab_day_65, N * I_ab_day_65, "y-")
+   # ax.plot(T_ab_day_65, N * R_ab_day_65, "b-")
+   # ax.plot(T_ab_day_65, N * (I_ab_day_65 + R_ab_day_65), "k-")
+    ax.plot(days, N * (I_data + R_data), "ko")
+    #ax.set_ylim(0, data_ger.confirmed.max())
+    ax.ticklabel_format(useOffset=False, style='plain')
+    pyplot.show()
+
+    exit(0)
+
 
     p0 = [r0]
     popt, pcov = scipy.optimize.curve_fit(
         fit_func,
-        xdata= T_data,
-        ydata= Y_data,
+        xdata=T_data,
+        ydata=Y_data,
         p0=p0)
-    print(popt)
+    print(popt, obj(popt))
+    #exit(0)
     best_model = create_model(*popt)
-    T, Y = best_model.integrate(y0, T_min, data["T"].max())
+    T, Y = best_model.integrate(y0, T_data[0], Y_data.max())
     S, E, I, R = SEIR.unravel(Y)
 
     fig, ax = pyplot.subplots()
@@ -72,7 +131,8 @@ if __name__ == "__main__":
     ax.plot(T, [i*N for i in I], "y")
     ax.plot(T, [r*N for r in R], "b")
     ax.plot(T, [(i+r)*N for i, r in zip(I, R)], "k")
-    ax.plot(data["T"], data.cases, "ko")
-    ax.set_ylim(0, data.cases.max())
+    #ax.plot(T_data, data_ger.confirmed, "ko")
+    #ax.set_ylim(0, data_ger.confirmed.max())
     ax.ticklabel_format(useOffset=False, style='plain')
     pyplot.show()
+

models/EpidemologicalModel.py
@@ -8,10 +8,11 @@ class EpidemologicalModel:
         raise NotImplementedError("This is an abstract method!")
 
     def integrate(self, y0, T0, Tmax):
-        solver = scipy.integrate.RK23(fun=self.f, t0=T0, y0=y0, t_bound=Tmax)
+        solver = scipy.integrate.Radau(fun=self.f, t0=T0, y0=y0, t_bound=Tmax, max_step=1)
 
         T, Y = list(), list()
         while solver.t < Tmax:
+            #print(solver.t)
             T.append(solver.t)
             Y.append(list(solver.y))
             solver.step()

models/SIR.py
@@ -4,40 +4,35 @@ import numpy as np
 from models.EpidemologicalModel import EpidemologicalModel
 
 
-class SEIR(EpidemologicalModel):
+class SIR(EpidemologicalModel):
     """
         https://de.wikipedia.org/wiki/SEIR-Modell
 
         Bez.    Einheit Beschreibung
         S(t)	1	Anteil der Anfälligen, engl. susceptible. Noch nicht infiziert.
-        E(t)	1	Anteil der Exponierten, engl. exposed. Infiziert, aber noch nicht infektiös.
         I(t)	1	Anteil der Infektiösen, engl. infectious.
         R(t)	1	Anteil der Erholten, engl. recovered oder resistant. Bzw. verstorben oder nach Symptomen in Quarantäne.
         t	    d	Zeit in Tagen, engl. time.
         β	   1/d	Transmissionsrate. Der Kehrwert ist die mittlere Zeit zwischen Kontakten.
         γ	   1/d	Gesundungsrate. Der Kehrwert ist die mittlere infektiöse Zeit.
-        a	   1/d	Der Kehrwert ist die mittlere Latenzzeit.
     """
-    def __init__(self, beta, gamma, a):
+    def __init__(self, beta, gamma):
         self.beta = beta
         self.gamma = gamma
-        self.a = a
 
     def f(self, t, y):
-        S, E, I, R = y
+        S, I, R = y
         return np.array([
             -self.beta * S * I,
-            self.beta * S * I - self.a * E,
-            self.a * E - self.gamma * I,
+            self.beta * S * I - self.gamma * I,
             self.gamma * I
         ])
 
     @staticmethod
     def unravel(Y):
-        S, E, I, R = [], [], [], []
-        for s, e, i, r in Y:
+        S, I, R = [], [], []
+        for s, i, r in Y:
             S.append(s)
-            E.append(e)
             I.append(i)
             R.append(r)
-        return np.array(S), np.array(E), np.array(I), np.array(R)
+        return np.array(S), np.array(I), np.array(R)